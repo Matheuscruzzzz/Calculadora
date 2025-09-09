943c6745a52a54fc4f71890903412983216fb31a
CRUZZ <matheus.cruz.souza@escola.pr.gov.br>
2025-09-09 12:13:44 -0300

    Add files via upload

--- matheus/script.js
+++ matheus/script.js
@@ -1,14 +1,27 @@
-document.getElementById('calcular');
+const calcular = document.getElementById('calcular');
 
 function imc(){
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado').value;
+    const resultado = document.getElementById
+    ('resultado');
 
     const valorIMC = (peso/(altura*altura)).toFixed(1);
 
-    resultado.textContent = `${nome}seu IMC é ${valorIMC}!`;
-}
 
-calcular.addEventListener('click',imc)
+
+    let clasificacao ="";
+
+    if (valorIMC < 18.5){
+        classificacao = "Abaixo do peso";
+    }else if(valorIMC < 25){
+        classificacao = "peso ideal";
+    }else if (valorIMC < 30){
+        clasificacao = "levemente acima do peso";  
+    }else{
+        clasificacao = "Obesidade";
+    }
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} , sua classificação é ${classificacao}!`;
+}
+calcular.addEventListener('click',imc);
